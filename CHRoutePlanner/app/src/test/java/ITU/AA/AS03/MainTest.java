6f580e2f38e95c6489542ff4a8709a4a7815986c
Marcus Skjold <marcusskjold@gmail.com>
2024-10-24 14:40:14 +0200

    Initalize Gradle Project

---
+++ CHRoutePlanner/app/src/test/java/ITU/AA/AS03/MainTest.java
@@ -0,0 +1,14 @@
+package ITU.AA.AS03;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+   @Test
+    public void testTest() {
+        Main.main(null);
+        assertTrue(true);
+    }
+}
